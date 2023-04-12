6ff575cf42957aa2260b3dd33b5b8a8d2100ec52
scubillos <cbasqbll95@gmail.com>
2023-04-11 23:47:00 -0500

    Ajustes app

--- app/config/logger.js
+++ app/config/logger.js
@@ -3,7 +3,6 @@ const pino = require('pino');
 require('dotenv').config();
 
 const log_level = process.env.LOG_LEVEL || 'info';
-const fs = require('fs');
 
 const logger = pino({
   level: log_level,
